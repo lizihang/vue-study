789f833a9a38a86d07730326a68696e55cf480f2
lizihang <lizihang@snsoft.com.cn>
2020-08-04 16:39:16 +0800

    update and add a component

--- webpack的使用/04-webpack配置vue/src/vue/App.vue
+++ webpack的使用/04-webpack配置vue/src/vue/App.vue
@@ -2,10 +2,13 @@
   <div>
     <h1 class="title">{{message}}</h1>
     <button @click="btnClick">按钮</button>
+    <cpn></cpn>
   </div>
 </template>
 
 <script>
+  import Cpn from "./Cpn.vue";
+
   export default {
     name: "App",
     data() {
@@ -17,12 +20,15 @@
       btnClick() {
         console.log("btnClick!");
       }
+    },
+    components: {
+      Cpn
     }
   }
 </script>
 
 <style scoped>
-  .title{
+  .title {
     color: green;
   }
 </style>
---
+++ webpack的使用/04-webpack配置vue/src/vue/Cpn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 class="title">{{cmessage}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Cpn",
+    data() {
+      return {
+        cmessage: "这是个组件"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    color: orange;
+  }
+</style>
